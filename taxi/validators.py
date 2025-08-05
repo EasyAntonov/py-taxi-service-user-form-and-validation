0e1dd5bf68143b7a09610850995784dc5148b085
Antonov Andrew <prestigemilitary@gmail.com>
2025-08-05 12:01:07 +0300

    corrected error messages

--- taxi/validators.py
+++ taxi/validators.py
@@ -11,11 +11,11 @@ def license_number_validator(license_number: str) -> AnyStr:
 
     if not license_number[:3].isalpha() or not license_number[:3].isupper():
         raise ValidationError(
-            "License number must start with a 3 uppercase letter!"
+            "License number must start with a 3 uppercase letter."
         )
 
     if not license_number[3:].isdigit():
         raise ValidationError(
-            "license number must end with 5 digits."
+            "License number must end with 5 digits."
         )
     return license_number
